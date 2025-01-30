2dc04fc10772de7aab8b6a5a90e7ebc2b593e2d0
sas-miles <132398659+sas-miles@users.noreply.github.com>
2025-01-29 21:30:01 -0500

    fixed build error fail data fetch for canvas

--- app/(main)/experience/layout.tsx
+++ app/(main)/experience/layout.tsx
@@ -1,16 +1,20 @@
 import { Suspense } from "react";
 import Canvas from "@/experience/Canvas";
 import { SanityLive } from "@/sanity/lib/live";
+import { fetchSanityScenes } from "../actions";
 
 export default async function ExperienceLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  const scenes = await fetchSanityScenes();
+  const mainScene = scenes[0]; // Or use appropriate logic to get the main scene
+
   return (
     <main className="relative h-screen w-screen">
       <Suspense fallback={<div>Loading...</div>}>
-        <Canvas>{children}</Canvas>
+        <Canvas scene={mainScene}>{children}</Canvas>
       </Suspense>
       <SanityLive />
     </main>
